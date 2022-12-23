5c86e629885869a8b715c47146951a27d9ba0590
yashwanth_ks <94818yashwanth@gmail.com>
2022-12-23 22:15:24 +0530

    stuck in gulp

--- config/environment.js
+++ config/environment.js
@@ -1,10 +1,42 @@
+const fs = require('fs');
+const rfs = require('rotating-file-stream')
+const path = require('path');
+
+const logDirectory = path.join(__dirname, '../production_logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+const accessLogStream = rfs.createStream('access.log',{
+    interval: '1d',
+    path: logDirectory
+});
 const development = {
     name: 'development',
-    asset_path: './assets'
+    asset_path: './assets',
+    session_cookie_key:'blahsomeone',
+    db:'codeial_development',
+    google_client_ID: "932000036472-b2joq33s7l2p2acep4e92pu787bhlo4n.apps.googleusercontent.com",
+    google_client_Secret:"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",
+    google_call_back_url:"http://localhost:8000/users/auth/google/callback",
+    jwt_secret:'codeial',
+    morgan:{
+        mode: 'dev',
+        options: {stream: accessLogStream}
+    }
 }
  const production = {
-    name: 'production'
+    name: 'production',
+    asset_path: process.env.CODEIAL_ASSET_PATH,
+    session_cookie_key:process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db:process.env.CODEIAL_DB,
+    google_client_ID: process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_Secret:process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_call_back_url:process.env.CODEIAL_GOOGLE_CALL_BACK_URL,
+    jwt_secret:process.env.CODEIAL_JWT_SECRET,
+    morgan:{
+        mode: 'combined',
+        options: {stream: accessLogStream}
+    }
  }
-
- module.exports = development;
+// eval converts string to expression
+ module.exports = eval(process.env.CODEIAL_ENVIRONMENT)==undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
  
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const env = require('./config/environment')
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const app  = express();
 const port = 8000;
@@ -28,6 +29,7 @@ console.log('chat server is listening on port 5000');
 const path = require('path');
 
 app.use(cors());
+if(env.name=='development'){
 app.use(sassMiddleware({
     src: path.join(__dirname, env.asset_path,'./scss'),
     dest: path.join(__dirname, env.asset_path,'./css'),
@@ -35,6 +37,7 @@ app.use(sassMiddleware({
     outputStyle: 'extended',
     prefix: '/css'
 }))
+}
 app.use(express.urlencoded({extended:true}));
 /* is a method inbuilt in express to recognize the incoming 
 Request Object as strings or arrays. This method is called as a 
@@ -43,18 +46,18 @@ app.use(cookieParser());
 app.use(express.static(env.asset_path));
 // make the uploads paths available to browser
 app.use('/uploads', express.static(__dirname + '/uploads'))
-
+app.use(logger(env.morgan.mode, env.morgan.options));
 app.use(expressLayouts);
 // extact style and scripts from subpages into the layout 
 app.set('layout extractStyles', true)
 app.set('layout extractScripts', true)
  
 app.set('view engine', 'ejs');
-app.set('views','./views')
+app.set('views','./views');
 
 app.use(session({
     name: 'codeial',
-    secret:'blahsomeone',
+    secret:env.session_cookie_key,
     saveUninitialized:false,
     resave:false,
     cookie:{
